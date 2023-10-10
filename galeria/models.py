67e3b5a756f287266a8ce3702e325b53825a9f93
Carlos Roberto <carlosrjhoe@gmail.com>
2023-10-10 14:53:17 -0300

    Django: criando models Cavaleiros, e sua persistência no ORM

--- galeria/models.py
+++ galeria/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Cavaleiro(models.Model):
+    nome = models.CharField(max_length=100, blank=False, null=False)
+    constelacao = models.CharField(max_length=100, blank=False, null=False)
+    historia = models.TextField()
+    imagem = models.ImageField(upload_to='assets/imagens/galeria/%Y%m',blank=True)
+
+    def __str__(self) -> str:
+        return f'{self.nome} - {self.constelacao}'
+
+    objects = models.Manager()
--- galeria/views.py
+++ galeria/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
+from .models import Cavaleiro
 
 # Create your views here.
 def index(request):
-    return render(request, 'galeria/index.html')
+    cavaleiro = Cavaleiro.objects.all()
+    context = {
+        'cavaleiros': cavaleiro
+    }
+    return render(request, 'galeria/index.html', context)
 
 def imagem(request):
     return render(request, 'galeria/imagem.html')
