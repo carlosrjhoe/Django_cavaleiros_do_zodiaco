93b7300d2bd8c4db43924fb26edf0bf0c7eaf4c1
Carlos Roberto <carlosrjhoe@gmail.com>
2023-10-10 21:36:23 -0300

    Django: Resolvido o problema da imagem dinamica do models

--- galeria/models.py
+++ galeria/models.py
@@ -5,7 +5,7 @@ class Cavaleiro(models.Model):
     nome = models.CharField(max_length=100, blank=False, null=False)
     constelacao = models.CharField(max_length=100, blank=False, null=False)
     historia = models.TextField()
-    imagem = models.ImageField(upload_to='assets/imagens/galeria/%Y%m',blank=True)
+    imagem = models.ImageField(blank=True, upload_to='galeria/%Y%m')
 
     def __str__(self) -> str:
         return f'{self.nome} - {self.constelacao}'
