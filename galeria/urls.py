bb0df7eb73f93f6dab0b4c1eba372d664ad5f000
Carlos Roberto <carlosrjhoe@gmail.com>
2023-10-19 20:56:09 -0300

    Django: Criando templates de login e cadastro do app de usuarios. E utilizando a boas práticas de programação DJANGO nas chamadas de rotas

--- galeria/urls.py
+++ galeria/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
+app_name = 'galeria'
+
 urlpatterns = [
     path('', views.index, name='index'),
     path('imagem/<int:foto_id>', views.imagem, name='imagem'),
--- usuarios/urls.py
+++ usuarios/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from .views import LoginTemplateView, CadastroTemplateView
 
+app_name = 'usuarios'
+
 urlpatterns = [
     path('login/', LoginTemplateView.as_view(), name='login'),
     path('cadastro/', CadastroTemplateView.as_view(), name='cadastro'),
